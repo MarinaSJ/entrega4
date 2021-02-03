e5802d27424c05c690b765139f642cefef596c67
Marina <msanginetojuca@gmail.com>
2021-02-03 17:00:35 -0300

    Teste

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+import Header from './components/Header/index';
+
+import Main from './components/Main/index';
+
+function Header(){
+  return(
+    <h1>Header</h1>
+  )
+}
+
+function App() {
+  return (
+      <>
+      <Header
+      
+      />
+        <div></div>
+      <Main/>
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from  'react';
+
+import './style.css';
+
+import '../Grid/index'
+
+function Main(){
+    return(
+
+    )
+}
+
+export default Main;
